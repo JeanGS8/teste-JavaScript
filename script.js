0e2e9b824f4d0e38055d3b13eb81e9720d2cb316
Jean <jean118877@hotmail.com>
2022-09-12 10:42:31 -0300

    Exercício concluido

---
+++ script.js
@@ -0,0 +1,29 @@
+const ageText = prompt('Type your age: ');
+const age = Number(ageText);
+const ageString = String(age);
+//const ageStrings = agr + ' ';
+
+if(age > 18){
+    document.write('então você é adulto!');
+}
+else if(age > 12){
+    document.write('então você é adolescente!');
+}
+else{
+    document.write('então você é criança!');
+}
+
+document.write('<br>');
+
+switch(age){
+    case (16): document.write('Já pode votar!');
+    break;
+
+    case (18): document.write('Já pode beber e dirigir, não ao mesmo tempo!');
+    break;
+
+    case (65): document.write('Já pode pegar ônibus de graça!')
+    break;
+
+    default: document.write('se lascou!');
+}
